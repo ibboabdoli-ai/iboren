7e21502ce04f9802503808989cb91d318b11c42f
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-19 20:02:34 +0200

    Enable visible text cleanup

--- app/template.tsx
+++ app/template.tsx
@@ -1,8 +1,10 @@
 import SeoInternalLinks from "./SeoInternalLinks";
+import TextCleanup from "./TextCleanup";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <TextCleanup />
       {children}
       <SeoInternalLinks />
     </>
