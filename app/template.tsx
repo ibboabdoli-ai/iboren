47be803e979cbec79d0eefb8a1f2116c89958586
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-20 23:26:35 +0200

    Mount global language switcher

--- app/template.tsx
+++ app/template.tsx
@@ -2,6 +2,7 @@ import BookingRutEnhancer from "./BookingRutEnhancer";
 import CinematicFrameLock from "./CinematicFrameLock";
 import HeaderLogo from "./HeaderLogo";
 import InternalLinkNormalizer from "./InternalLinkNormalizer";
+import LanguageSwitcher from "./LanguageSwitcher";
 import PerformanceHints from "./PerformanceHints";
 import SeoInternalLinks from "./SeoInternalLinks";
 
@@ -13,6 +14,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <CinematicFrameLock />
       <BookingRutEnhancer />
       <PerformanceHints />
+      <LanguageSwitcher />
       {children}
       <SeoInternalLinks />
     </>
