a7ac6560a123e5c9c95c1109a6fba7b5c5eabda0
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-20 11:37:32 +0200

    Enable booking response guard

--- app/template.tsx
+++ app/template.tsx
@@ -1,3 +1,4 @@
+import BookingResponseGuard from "./BookingResponseGuard";
 import InternalLinkNormalizer from "./InternalLinkNormalizer";
 import SeoInternalLinks from "./SeoInternalLinks";
 
@@ -5,6 +6,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <InternalLinkNormalizer />
+      <BookingResponseGuard />
       {children}
       <SeoInternalLinks />
     </>
