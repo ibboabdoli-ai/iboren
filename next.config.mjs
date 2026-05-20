08181b16ffaeae58b70ab7153cd13689e3026c1f
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-20 11:29:22 +0200

    Fix Next config redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     formats: ["image/avif", "image/webp"]
+  },
+  async redirects() {
+    return [
+      { source: "/jobb", destination: "/jobba-hos-oss", permanent: true },
+      { source: "/om-iboren", destination: "/om-oss", permanent: true },
+      { source: "/hemstadning", destination: "/tjanster/hemstadning", permanent: true },
+      { source: "/flyttstadning", destination: "/tjanster/flyttstadning", permanent: true },
+      { source: "/kontorsstadning", destination: "/tjanster/kontorsstadning", permanent: true },
+      { source: "/fonsterputs", destination: "/tjanster/fonsterputs", permanent: true }
+    ];
   }
 };
 
