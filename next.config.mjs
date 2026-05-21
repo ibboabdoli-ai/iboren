93b4fbd3e30409d0ae1db5dfd7502e71739d4a3b
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-21 16:49:15 +0200

    Revert Next mjs config to original

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,6 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     formats: ["image/avif", "image/webp"]
-  },
-  async redirects() {
-    return [
-      { source: "/booking", destination: "/boka", permanent: true },
-      { source: "/jobb", destination: "/jobba-hos-oss", permanent: true },
-      { source: "/om-iboren", destination: "/om-oss", permanent: true },
-      { source: "/hemstadning", destination: "/tjanster/hemstadning", permanent: true },
-      { source: "/flyttstadning", destination: "/tjanster/flyttstadning", permanent: true },
-      { source: "/kontorsstadning", destination: "/tjanster/kontorsstadning", permanent: true },
-      { source: "/fonsterputs", destination: "/tjanster/fonsterputs", permanent: true }
-    ];
   }
 };
 
