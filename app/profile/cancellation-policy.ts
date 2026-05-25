0fc936ead84e984c4245814ec153b8b29ad3d9c1
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-25 11:53:55 +0200

    Document customer cancellation cutoff

---
+++ app/profile/cancellation-policy.ts
@@ -0,0 +1,2 @@
+export const CUSTOMER_CANCELLATION_CUTOFF_HOURS = 48;
+export const CUSTOMER_CANCELLATION_POLICY_TEXT = "Online-avbokning är möjlig senast 48 timmar före bokat datum. Vid kortare varsel behöver kunden kontakta Iboren.";
