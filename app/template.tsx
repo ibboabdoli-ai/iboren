a8fc925b297d8879ce3fe58a9c2cdbcf4b41f33f
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-05-27 21:32:09 +0200

    Remove duplicate floating language switch

--- app/template.tsx
+++ app/template.tsx
@@ -4,7 +4,6 @@ import EnglishLinkNormalizer from "./EnglishLinkNormalizer";
 import EnglishProfileLinkNormalizer from "./EnglishProfileLinkNormalizer";
 import HeaderLogo from "./HeaderLogo";
 import InternalLinkNormalizer from "./InternalLinkNormalizer";
-import LanguageSwitcher from "./LanguageSwitcher";
 import PerformanceHints from "./PerformanceHints";
 import SeoInternalLinks from "./SeoInternalLinks";
 import SwedishBookingCopyFix from "./SwedishBookingCopyFix";
@@ -20,7 +19,6 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <CinematicFrameLock />
       <BookingRutEnhancer />
       <PerformanceHints />
-      <LanguageSwitcher />
       {children}
       <SeoInternalLinks />
     </>
