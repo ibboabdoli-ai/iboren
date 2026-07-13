de3652d6d0e585d24f19c417c2e574108ad69fa2
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-07-13 21:49:55 +0200

    feat: track booking funnel events

--- app/components/home/AnalyticsBookingLink.tsx
+++ app/components/home/AnalyticsBookingLink.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { trackSiteEvent } from "../../lib/analytics";
 
 export default function AnalyticsBookingLink() {
-  return <Link href="/priser#pris-kalkylator" onClick={() => trackSiteEvent("quote_cta_click")} className="btn-primary">Få pris direkt</Link>;
+  return <Link href="/priser#pris-kalkylator" data-site-analytics-event="quote_cta_click" className="btn-primary">Få pris direkt</Link>;
 }
