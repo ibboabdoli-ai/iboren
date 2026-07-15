ee8f33b1f0958fa6dffc0bfe330b99fe79b1f748
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-07-15 13:59:58 +0200

    Restore cinematic scroll playback

--- app/template.tsx
+++ app/template.tsx
@@ -3,6 +3,7 @@ import BookingRutEnhancer from "./BookingRutEnhancer";
 import BookingSnapshotValidationShield from "./BookingSnapshotValidationShield";
 import BookingSubmissionSnapshot from "./BookingSubmissionSnapshot";
 import BookingValueSanityGuard from "./BookingValueSanityGuard";
+import CinematicFrameLock from "./CinematicFrameLock";
 import EnglishHomeServicesEnhancer from "./EnglishHomeServicesEnhancer";
 import EnglishLinkNormalizer from "./EnglishLinkNormalizer";
 import EnglishProfileLinkNormalizer from "./EnglishProfileLinkNormalizer";
@@ -24,6 +25,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <HeaderLogo />
       <HomeMobileLanguageButton />
       <EnglishHomeServicesEnhancer />
+      <CinematicFrameLock />
       <BookingAutofillSafetyGuard />
       <BookingRutEnhancer />
       <BookingSnapshotValidationShield />
